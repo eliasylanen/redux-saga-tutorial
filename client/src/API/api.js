d9737b73a4c18a6a371e551b4c2a962d9363e27f
Elias Ylänen <elias.ylanen@gmail.com>
2017-04-28 14:06:18 +0300

    Define API endpoints

---
+++ client/src/API/api.js
@@ -0,0 +1,54 @@
+const FLICKR_API_KEY = 'a46a979f39c49975dbdd23b378e6d3d5';
+const SHUTTER_CLIENT_ID = '3434a56d8702085b9226';
+const SHUTTER_CLIENT_SECRET = '7698001661a2b347c2017dfd50aebb2519eda578';
+
+// Basic Authentication for accessing Shutterstock API
+const basicAuth = () =>
+  'Basic '.concat(window.btoa(`${SHUTTER_CLIENT_ID}:${SHUTTER_CLIENT_SECRET}`));
+
+const authParameters = {
+  headers: {
+    Authorization: basicAuth(),
+  },
+};
+
+/**
+* Description [Access Shutterstock search endpoint for short videos]
+* @params { String } searchQuery
+* @return { Array } 
+*/
+export const shutterStockVideos = async searchQuery => {
+  const SHUTTERSTOCK_API_ENDPOINT = `https://api.shutterstock.com/v2/videos/search?
+  query=${searchQuery}&page=1&per_page=10`;
+
+  const data = await fetch(SHUTTERSTOCK_API_ENDPOINT, authParameters);
+  const json = await data.json();
+
+  console.log(json);
+
+  return json.data.map(({ id, assets, descritpion }) => ({
+    id,
+    mediaUrl: assets.preview_mp4.url,
+    descritpion,
+  }));
+};
+
+/**
+* Description [Access Flickr search endpoint for photos]
+* @params { String } searchQuery
+* @return { Array } 
+*/
+export const flickrImages = async searchQuery => {
+  const FLICKR_API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.photos.search&text=${searchQuery}&api_key=${FLICKR_API_KEY}&format=json&nojsoncallback=1&per_page=10`;
+
+  const data = await fetch(FLICKR_API_ENDPOINT);
+  const json = await data.json();
+
+  console.log(json);
+
+  return json.photos.photo.map(({ farm, server, id, secret, title }) => ({
+    id,
+    title,
+    mediaUrl: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`,
+  }));
+};
