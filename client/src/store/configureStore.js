bf85c4eb2185b5eb0c77fc680c55c2f39e49dc25
Elias Ylänen <elias.ylanen@gmail.com>
2017-05-03 16:46:36 +0300

    Make store creation clearer

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
@@ -11,15 +11,16 @@ import rootSaga from '../sagas';
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
 
-  return {
-    ...createStore(
-      rootReducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-      applyMiddleware(sagaMiddleware)
-    ),
-    runSaga: sagaMiddleware.run(rootSaga),
-  };
+  const enhancer = compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  const store = createStore(rootReducer, enhancer);
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
 };
 
 export default configureStore;
